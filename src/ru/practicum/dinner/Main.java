d3a9ceffbe2b41d4d46a52ca76c46ece45f13aa0
diart <diart@DESKTOP-GRU8VQP>
2025-09-29 15:49:33 +0200

    cosmetic fixes

--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -57,20 +57,20 @@ public class Main {
 
         //реализуйте ввод типов блюд
         ArrayList<String> selectedTypes = new ArrayList<>();
-        while (!nextItem.isEmpty()) { //варианты вводит пользователь
-            if (dinnerConstructor.checkType(nextItem)) { //но вы должны проверить, существуют ли эти блюда в хранилище с помощью метода DinnerConstructor checkType
-                selectedTypes.add(nextItem); //выбранное блюдо добавьте в список вариантов
+        while (!nextItem.isEmpty()) {
+            if (dinnerConstructor.checkType(nextItem)) {
+                selectedTypes.add(nextItem);
             } else {
                 System.out.println("Такой тип блюд мы еще не умеем готовить. Попробуйте что-нибудь другое!");
             }
-            nextItem = scanner.nextLine(); //перейдите к следующему пункту ввода пользователя
+            nextItem = scanner.nextLine();
         }
 
-        // сгенерируйте комбинации блюд и выведите на экран
-        ArrayList<String> generatedCombos = dinnerConstructor.generateCombos(numberOfCombos, selectedTypes); //сгенерируйте варианты комбинаций блюд с помощью метода DinnerConstructor generateCombos
-        for (int i = 0 ; i < numberOfCombos; i++) {
+
+        ArrayList<String> generatedCombos = dinnerConstructor.generateCombos(numberOfCombos, selectedTypes);
+        for (int i = 0; i < numberOfCombos; i++) {
             System.out.println("Комбинация " + i);
-            System.out.println(generatedCombos.get(i)); //выведите каждый элемент получившейся комбинации
+            System.out.println(generatedCombos.get(i));
         }
     }
 }
